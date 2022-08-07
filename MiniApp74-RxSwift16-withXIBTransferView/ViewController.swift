9685a22d24329ae35340ec21286ab024d9bc5875
Kota <maedakoutaa@gmail.com>
2022-08-07 12:37:41 +0900

    Initial Commit

---
+++ MiniApp74-RxSwift16-withXIBTransferView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp74-RxSwift16-withXIBTransferView
+//
+//  Created by 前田航汰 on 2022/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
